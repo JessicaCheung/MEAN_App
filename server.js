b0f7a5fde56974001b4d6ee9ea9a4d2c0e539819
JessicaCheung <jcheung.cs@gmail.com>
2015-11-04 17:12:21 -0800

    Static DB

--- server.js
+++ server.js
@@ -3,5 +3,26 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/contactlist', function(req, res) {
+	console.log("I received a GET request");
+	person1 = {
+		name: 'Tim',
+		email: 'Tim@oisdjf.com',
+		number: '(111) 111-1111'
+	};
+	person2 = {
+		name: 'Tom',
+		email: 'Tom@oisdjf.com',
+		number: '(111) 111-1121'
+	};
+	person3 = {
+		name: 'Bloop',
+		email: 'Bloop@oisdjf.com',
+		number: '(111) 311-1111'
+	};
+	var contactlist = [person1, person2, person3];
+	res.json(contactlist);
+});
+
 app.listen(8080);
 console.log("Server running on port 8080");
